2525b7e9613948c4903063ed19537dab97dfd930
ChuHanna <a33931@thanglong.edu.vn>
2021-09-25 02:51:58 +0700

    Chapter_7: Sign up

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
     get "/home", to: "static_pages#home"
     get "/help", to:"static_pages#help"
     get "/signup", to: "users#new"
+    resources :users, except: :new
   end
 end
