f88bcebe91d5bc414547f798c0aa9bf6f0f6451b
ChuHanna <a33931@thanglong.edu.vn>
2021-09-27 15:47:47 +0700

    Chapter 8: Basic login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   # de chuan hoa su ton tai cho name va email
   VALID_EMAIL_REGEX = Settings.valid
 
@@ -13,6 +14,30 @@ class User < ApplicationRecord
   # if :password chi validate password chi khi password thay doi khi cap nhap
   has_secure_password
   validates :password, presence: true, length: {minimum: Settings.min_pass}
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute :remember_digest, User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
+  class << self
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+
+    def digest string
+      cost = if ActiveModel::SecurePassword.min_cost
+               BCrypt::Engine.min_cost
+             else
+               BCrypt::Engine.cost
+             end
+      BCrypt::Password.create string, cost: cost
+    end
+  end
 
   private
   def downcase_email
--- db/schema.rb
+++ db/schema.rb
@@ -6,12 +6,19 @@
 # db:schema:load`. When creating a new database, `rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
-ActiveRecord::Schema.define(version: 2021_09_24_015707) do
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_09_28_035538) do
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_vietnamese_ci", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.string "remember_digest"
   end
+
 end
